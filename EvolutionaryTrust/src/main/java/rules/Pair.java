9f6b486d08b5824db360b9c1d57c81ab6d6cf16f
Gopikrishna Yaramothu <gopikrishnacse36@gmail.com>
2020-04-14 09:08:40 +0530

    Replaced with Wrapper Classes in Pair Entity

--- EvolutionaryTrust/src/main/java/rules/Pair.java
+++ EvolutionaryTrust/src/main/java/rules/Pair.java
@@ -2,19 +2,19 @@ package rules;
 
 public class Pair {
 
-    int score1;
-    int score2;
+    Integer score1;
+    Integer score2;
 
-    public Pair(int score1, int score2) {
+    public Pair(Integer score1, Integer score2) {
         this.score1 = score1;
         this.score2 = score2;
     }
 
-    public int getScore1() {
+    public Integer getScore1() {
         return score1;
     }
 
-    public int getScore2() {
+    public Integer getScore2() {
         return score2;
     }
 }
--- EvolutionaryTrust/src/test/java/rules/RuleEngineTest.java
+++ EvolutionaryTrust/src/test/java/rules/RuleEngineTest.java
@@ -13,40 +13,40 @@ public class RuleEngineTest {
     public void testCheatCheat() {
         int[] expectedScores = {0,0};
         Pair scores = RuleEngine.returnScoresUsingRules(CHEAT,CHEAT);
-        assertEquals(0,scores.getScore1());
-        assertEquals(0,scores.getScore2());
+        assertEquals(Integer.valueOf(0),scores.getScore1());
+        assertEquals(Integer.valueOf(0),scores.getScore2());
     }
 
     @Test
     public void testCooperateCooperate() {
         int[] expectedScores = {2,2};
         Pair scores = RuleEngine.returnScoresUsingRules(COOPERATE,COOPERATE);
-        assertEquals(2,scores.getScore1());
-        assertEquals(2,scores.getScore2());
+        assertEquals(Integer.valueOf(2),scores.getScore1());
+        assertEquals(Integer.valueOf(2),scores.getScore2());
     }
 
     @Test
     public void testCheatCooperate() {
         int[] expectedScores = {3,-1};
         Pair scores = RuleEngine.returnScoresUsingRules(CHEAT,COOPERATE);
-        assertEquals(3,scores.getScore1());
-        assertEquals(-1,scores.getScore2());
+        assertEquals(Integer.valueOf(3),scores.getScore1());
+        assertEquals(Integer.valueOf(-1),scores.getScore2());
     }
 
     @Test
     public void testCooperateCheat() {
         int[] expectedScores = {-1,3};
         Pair scores = RuleEngine.returnScoresUsingRules(COOPERATE,CHEAT);
-        assertEquals(-1,scores.getScore1());
-        assertEquals(3,scores.getScore2());
+        assertEquals(Integer.valueOf(-1),scores.getScore1());
+        assertEquals(Integer.valueOf(3),scores.getScore2());
     }
 
     @Test
     public void testInvalidMoves() {
         int[] expectedScores = {-1,-1};
         Pair scores = RuleEngine.returnScoresUsingRules("TEST1","TEST@");
-        assertEquals(-1,scores.getScore1());
-        assertEquals(-1,scores.getScore2());
+        assertEquals(Integer.valueOf(-1),scores.getScore1());
+        assertEquals(Integer.valueOf(-1),scores.getScore2());
     }
 
 }
